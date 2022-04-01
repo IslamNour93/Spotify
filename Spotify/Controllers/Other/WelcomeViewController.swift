b793292edf99542a054aa7065c10b0a870b4b9a3
Islam NourEldin <99802390+IslamNour93@users.noreply.github.com>
2022-04-01 08:33:41 -0700

    working on auth

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    private let signInButton: UIButton = {
+        let signInButton = UIButton()
+        signInButton.backgroundColor = .white
+        signInButton.setTitle("Sign In with Spotify", for: .normal)
+        signInButton.setTitleColor(.systemBlue, for: .normal)
+        return signInButton
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
         title = "Spotify"
+        view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        signInButton.frame = CGRect(x: 20,
+                                    y: view.height-50-view.safeAreaInsets.bottom,
+                                    width: view.width-40,
+                                    height: 50)
+    }
+    @objc func didTapSignIn(){
+        let authVc = AuthViewController()
+        authVc.navigationItem.largeTitleDisplayMode = .never
+        authVc.completionHandler = { [weak self] success in
+            DispatchQueue.main.async {
+                self?.handleSignIn(success: success)
+            }
+        }
+        navigationController?.pushViewController(authVc, animated: false)
+    }
+    private func handleSignIn(success: Bool){
+        
     }
 }
--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -8,9 +8,22 @@
 import Foundation
 
 final class AuthManager{
-    
     static let shared = AuthManager()
     
+    struct Constants{
+        static let clientID = "fc6cfea21b8a4c888d915405ef683688"
+        static let clientSecret = "4b69e1944532489bbe15415cef788816"
+    }
+    private init(){
+        
+    }
+    public var signInUrl: URL?{
+        let redirectUri = "https://www.iosacademy.io/"
+        let scopes = "user-read-private"
+        let baseUrl = "https://accounts.spotify.com/authorize"
+        let url = "\(baseUrl)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectUri)&show_dialog=TRUE"
+        return URL(string: url)
+    }
     var isSigned : Bool{
         return false
     }
@@ -29,5 +42,16 @@ final class AuthManager{
     
     private  var shouldReturnToken:Bool{
         return false
+    }
+    public func exchangeCodeForToken(code:String,Completion :@escaping ((Bool)->Void)){
+        //Get Token
+    }
+    
+    public func refreshAccessToken(){
+        
+    }
+    
+    public func cacheToken(){
+        
     }
 }
